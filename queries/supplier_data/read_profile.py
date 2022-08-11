3b7337857161f85d83472a326f5285fa65db28ad
Douglas Campbell <doug.campbell@netwisedata.com>
2022-08-10 17:01:30 -0700

    remove eyewise specific analysis

--- queries/supplier_data/read_profile.py
+++
@@ -1,4 +0,0 @@
-import pstats
-import sys
-p = pstats.Stats(sys.argv[1])
-p.strip_dirs().sort_stats('cumtime').print_callers('io.open').print_callers('processTemplateVar').print_stats()
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -o nounset
 
 cd $(dirname $0)
 
@@ -8,36 +9,20 @@ cd $(dirname $0)
 
 docker build -t $imagename:$current_commit .
 
-#if [ -z $SRC_DIR ]; then
-#  echo "Must specify SRC_DIR. Path where a user's git repos are present."
-#  exit 1;
-#fi
-#echo "SRC: $SRC_DIR"
-
-if [[ ! -f ~/.aws/mfa ]]
-then
-    echo Please make you have a valid mfa credentials file
-    exit 1
-fi
-#SECRET=${SECRET:-k8app__bq-third-party__staging__bqm2-json__gcloud-private-key}
-# get creds
-#mkdir -p /tmp/creds
-#docker run -ti -v ~/.aws:/root/.aws -v /tmp/creds:/tmp/creds -e AWS_SHARED_CREDENTIALS_FILE=/root/.aws/mfa -e AWS_DEFAULT_REGION=us-east-1 docker.io/stops/secrets-manager-v2:5b20bdefaa python /src/client.py ${SECRET} /tmp/creds/gcloud-private-key
-
-
 if [[ ! -f ~/.vimrc ]]
 then
     touch ~/.vimrc
 fi
 
+echo mounting ${QUERIES} to ${MOUNT}
 docker run -e GOOGLE_APPLICATION_CREDENTIALS=/gcloud-private-key \
 -e AWS_SHARED_CREDENTIALS_FILE=/root/.aws/mfa \
 -v ~/Downloads/eyeota-test-37d40f8af2b4.json:/gcloud-private-key \
 -v ~/.vimrc:/root/.vimrc \
+-v ${QUERIES}:${MOUNT} \
 --name bqm2 -v ~/.config:/root/.config \
 -v $(pwd)/python:/python \
 -v $(pwd)/test:/test \
--v $(pwd)/queries:/queries \
 -v $(pwd)/int-test:/int-test \
 -v ~/.aws:/root/.aws \
 -ti --rm $imagename:$current_commit $@
